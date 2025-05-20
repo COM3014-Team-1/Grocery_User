f9bd0f3375bdfac85a47e7ea59fed9b5c8972784
Alice <ms04773@surrey.ac.uk>
2025-05-20 20:22:27 +0100

    add deployment setting

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-very-secret-key')
     # Update the URL with new PostgreSQL user "htetaung" and database "user_profiles"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://htetaung:1131992Ha@localhost:5432/user_profiles')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:SHMIgtBBk1zQKYhMSmDX@database-1.c3u0oys8ik2c.eu-west-2.rds.amazonaws.com:5432/user_profiles')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
