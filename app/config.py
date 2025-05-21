a3e0224da70ff0c0ecf73e2fba9a8ff732ab97d6
Alice <ms04773@surrey.ac.uk>
2025-05-21 09:38:30 +0100

    update secret key

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-very-secret-key')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'GT6kS1_ascJ2lv8LvL-QIiXGzsBjJvGmFJri5YmKQU6wBamEn3kVjN4YsyTKBTQlH-vz--tLRi-HqToRuHHWeQ==')
     # Update the URL with new PostgreSQL user "htetaung" and database "user_profiles"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:SHMIgtBBk1zQKYhMSmDX@database-1.c3u0oys8ik2c.eu-west-2.rds.amazonaws.com:5432/user_profiles')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
